8fb1410e9d5c6bc2945176d2463dc5e444e7205a
José de Azevedo <josejoaquimrizzo@hotmail.com>
2021-03-25 20:14:31 +0000

    +funcionalSemSupplierHandler

--- boozeApp/src/main/java/com/boozeApp/boozeApp/controller/SupplierController.java
+++ boozeApp/src/main/java/com/boozeApp/boozeApp/controller/SupplierController.java
@@ -1,8 +1,6 @@
 package com.boozeApp.boozeApp.controller;
 
 
-import com.boozeApp.boozeApp.businessRules.SupplierHandler;
-import com.boozeApp.boozeApp.model.dto.ProductDTO;
 import com.boozeApp.boozeApp.model.dto.SupplierDTO;
 import com.boozeApp.boozeApp.repository.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 
 @RestController
